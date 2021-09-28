2fa91eca62df8cd4a619041095c1fc69423b66a1
ahmedelsayedsaleh <ahmed0elsayed0saleh@gmail.com>
2021-09-28 12:59:15 +0200

    incubator: v1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Footer, Header, Hero } from "./layout";
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter basename={"/incubator"}>
+      <Router>
         <Header />
         <Hero />
         <Footer />
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
